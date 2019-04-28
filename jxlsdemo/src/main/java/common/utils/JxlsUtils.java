157a0bf88e4f87eb878a4e27c5d9537a5f4d5bc0
datahan <datawisher@gmail.com>
2019-04-28 22:01:52 +0800

    jxlsdemo项目完成

--- jxlsdemo/src/main/java/common/utils/JxlsUtils.java
+++ jxlsdemo/src/main/java/common/utils/JxlsUtils.java
@@ -1,21 +1,15 @@
 package common.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.jxls.common.Context;
 import org.jxls.expression.JexlExpressionEvaluator;
 import org.jxls.transform.Transformer;
 import org.jxls.util.JxlsHelper;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 
@@ -24,7 +18,7 @@ import org.jxls.util.JxlsHelper;
  */
 public class JxlsUtils{
 	
-	private static final String TEMPLATE_PATH="jxls-template";
+	private static final String TEMPLATE_PATH= "jxls-template";
 	
 	public static void exportExcel(InputStream is, OutputStream os, Map<String, Object> model) throws IOException{
         Context context = new Context();
@@ -36,28 +30,29 @@ public class JxlsUtils{
         JxlsHelper jxlsHelper = JxlsHelper.getInstance();
         Transformer transformer  = jxlsHelper.createTransformer(is, os);
         JexlExpressionEvaluator evaluator = (JexlExpressionEvaluator)transformer.getTransformationConfig().getExpressionEvaluator();
-        Map<String, Object> funcs = new HashMap<String, Object>();
-        funcs.put("utils", new JxlsUtils());    //添加自定义功能
+        Map<String, Object> funcs = new HashMap<>(1);
+
+        //添加自定义功能
+        funcs.put("utils", new JxlsUtils());
         evaluator.getJexlEngine().setFunctions(funcs);
         jxlsHelper.processTemplate(context, transformer);
 	}
 
-    public static void exportExcel(File xls, File out, Map<String, Object> model) throws FileNotFoundException, IOException {
+    public static void exportExcel(File xls, File out, Map<String, Object> model) throws IOException {
             exportExcel(new FileInputStream(xls), new FileOutputStream(out), model);
     }
-    
-    public static void exportExcel(String templateName, OutputStream os, Map<String, Object> model) throws FileNotFoundException, IOException {
+
+    public static void exportExcel(String templateName, OutputStream os, Map<String, Object> model) throws IOException {
     	File template = getTemplate(templateName);
     	if(template!=null){
         	exportExcel(new FileInputStream(template), os, model);	
     	}
     }
     
-    
     //获取jxls模版文件
-
     public static File getTemplate(String name){
         String templatePath = JxlsUtils.class.getClassLoader().getResource(TEMPLATE_PATH).getPath();
+
         File template = new File(templatePath, name);
         if(template.exists()){
             return template;
--- jxlsdemo/src/main/java/jxls/demo/guide/ObjectCollectionDemo.java
+++ jxlsdemo/src/main/java/jxls/demo/guide/ObjectCollectionDemo.java
@@ -1,21 +1,15 @@
 package jxls.demo.guide;
 
+import common.utils.JxlsUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import common.utils.JxlsUtils;
+import java.util.*;
 
 /**
  * Object collection output demo
@@ -28,8 +22,8 @@ public class ObjectCollectionDemo {
     	logger.info("Running Object Collection demo");
     	
         List<Employee> employees = generateSampleEmployeeData();
-        OutputStream os = new FileOutputStream("target/object_collection_output.xls");
-        Map<String , Object> model=new HashMap<String , Object>();
+        OutputStream os = new FileOutputStream("D:/object_collection_output.xls");
+        Map<String , Object> model=new HashMap<>(2);
         model.put("employees", employees);
         model.put("nowdate", new Date());
         JxlsUtils.exportExcel("object_collection_template.xls", os, model);
@@ -37,7 +31,7 @@ public class ObjectCollectionDemo {
     }
 
     public static List<Employee> generateSampleEmployeeData() throws ParseException {
-        List<Employee> employees = new ArrayList<Employee>();
+        List<Employee> employees = new ArrayList<>();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MMM-dd", Locale.US);
         employees.add( new Employee("Elsa", dateFormat.parse("1970-Jul-10"), 1500, 0.15) );
         employees.add( new Employee("Oleg", dateFormat.parse("1973-Apr-30"), 2300, 0.25) );
